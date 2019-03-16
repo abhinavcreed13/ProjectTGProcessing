f7c0ff4adf4ed159f7f940168346a51e607462ad
Abhinav Sharma <abhinavcs13@gmail.com>
2019-03-16 11:54:44 +1100

    Adding DataPreProcessing Structure

---
+++ lib/datapreprocessing.py
@@ -0,0 +1,10 @@
+class DataPreProcessing:
+
+    #1
+    def rem_missing_coordinates(self,tw_data):
+        return tw_data
+
+    #2,3
+    def add_loc_factor(self,tw_data):
+        #massaged twitter data - new structure
+        return tw_data
